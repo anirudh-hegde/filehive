8e0b79f5b346899a45b7d0554469cf9da9af2851
anirudh-hegde <hegdeanirudh2003@gmail.com>
2024-12-17 20:56:28 +0530

    Initial commit

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,15 +1,17 @@
-import './App.css';
+import './App.css'
 import FolderTree from './components/FolderTree.jsx'
 function App () {
   return (
-    <div className='flex justify-center items-center bg-gradient-to-r from-slate-800 to-slate-950 w-full'>
-      <div className='bg-black shadow-2xl p-5 rounded-2xl w-[700px]'>
-        <h1 className='mb-6 font-bold font-mono text-5xl text-center'>
-          Folder Hierarchy
-        </h1>
-        <FolderTree />
+    <main className='w-full h-screen dark:text-white'>
+      <div className='flex justify-center items-center border-green bg-black my-[10vw] w-auto'>
+        <div className='border-4 border-indigo-600 bg-grey shadow-2xl p-5 rounded-2xl w-[700px]'>
+          <h1 className='mb-6 font-bold font-mono text-5xl text-center text-white'>
+            Folder Hierarchy
+          </h1>
+          <FolderTree className='bg-black text-white' />
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
 
