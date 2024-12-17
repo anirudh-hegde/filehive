5fb91924853583b4fcde41fecf4be751e4aba4f4
anirudh-hegde <hegdeanirudh2003@gmail.com>
2024-12-17 23:50:33 +0530

    XXX

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import FolderTree from './components/FolderTree.jsx'
 function App () {
   return (
-    <main className='w-full h-screen dark:text-white'>
+    <main className='w-full h-screen dark:text-black'>
       <div className='flex justify-center items-center border-green bg-black my-[10vw] w-auto'>
         <div className='border-4 border-indigo-600 bg-grey shadow-2xl p-5 rounded-2xl w-[700px] cypiu'>
           <h1 className='mb-6 font-bold font-mono text-5xl text-center text-white'>
