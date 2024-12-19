28851bdb4ef2c53b59d4afa6c6a5431728e5f279
anirudh-hegde <hegdeanirudh2003@gmail.com>
2024-12-19 09:01:17 +0530

    XXX

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import '../index.css'
-import './frontend/index.css'
-import App from './frontend/App.jsx'
+import '/frontend/src/index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
